65f9727797f74be8b14b6955c7891563998f4620
f wrobel <ceccawrobel@gmail.com>
2019-06-21 17:57:04 -0700

    complete for-loop to include pong and pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     var count = parseInt($("input#inputNumber").val());
 
     event.preventDefault();
-    // console.log();
-    // alert(count);
     $("ul#outputList").text("");
 
     for (var index = 1; index <= count; index += 1) {
-      if (index % 3 === 0) {
+      if (index % 15 === 0) {
+        $("ul#outputList").append("<li>ping pong</li>")
+      } else if (index % 5 === 0) {
+        $("ul#outputList").append("<li>pong</li>")
+      } else if (index % 3 === 0) {
         $("ul#outputList").append("<li>ping</li>")
       } else {
         $("ul#outputList").append("<li>" + index + "</li>");
